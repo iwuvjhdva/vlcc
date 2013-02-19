b170de3eb497c9e7f43e382320051c588d57686d
Konstantin Alexandrov <iwuvjhdva@gmail.com>
2013-02-19 22:41:22 +0400

    configure && make && make install execution implemented

--- vlcc/build.py
+++ vlcc/build.py
@@ -13,6 +13,28 @@ from .conf import config
 __all__ = ['build']
 
 
+def build_state(state):
+    """Updates the build state in case of successful execution of the
+    decorated method.
+    """
+
+    def _build_state(method):
+        def __build_state(*args, **kwargs):
+            result = method(*args, **kwargs)
+
+            states = ['jail_created',
+                      'source_unpacked',
+                      'configured',
+                      'compiled',
+                      'installed']
+
+            assert state in states
+
+            return result
+        return __build_state
+    return _build_state
+
+
 class Builder(object):
     """Builder class to create a chroot jail with debootstrap and building a
     VLC version.
@@ -24,9 +46,10 @@ class Builder(object):
         self.version = version
         self.build_logger = logger.getChild(version)
         self.build_logger.setLevel(logger.level)
-        self.version_config = config['versions'][version]
         self.log_dir = os.path.join(options.build_dir, 'log-' + version)
-        self.build_dir = os.path.join(options.build_dir, 'jail-' + version)
+        self.version_config = config['versions'][version]
+        self.chroot_dir = os.path.join(options.build_dir, 'jail-' + version)
+        self.chroot_src_path = os.path.join('/usr/local/src', 'vlc-' + version)
 
         try:
             os.makedirs(self.log_dir)
@@ -36,21 +59,70 @@ class Builder(object):
     def debug_command(self, command):
         self.build_logger.debug("Executing `{0}`".format(" ".join(command)))
 
-    def start_download(self):
-        params = dict(
-            url=config['download_url'],
-            ver=self.version,
-            ext='xz' if self.version[0] == '2' else 'bz2',
-        )
+    def exec_command(self, command, log_to=None, action=None):
+        """Executes a command.
 
-        self.download_url = ("{0[url]}/{0[ver]}/vlc-{0[ver]}.tar.{0[ext]}"
-                             .format(params))
+        @param command: command string
+        @param log_to: log file name
+        @param action: action string to log
+        """
 
-        command = ['wget', '-c', '-p', '-P',
-                   options.build_dir, self.download_url]
+        exec_log_path = log_to and os.path.join(self.log_dir, log_to)
+
+        if action is not None:
+            message = ("{0} VLC" +
+                       ("" if log_to is None else ", see {1} for details"))
+            self.build_logger.info(message.format(action.capitalize(),
+                                                  exec_log_path))
 
         self.debug_command(command)
-        self.download_proc = subprocess.Popen(command)
+
+        if log_to is None:
+            retcode = subprocess.call(command)
+        else:
+            with open(exec_log_path, 'wt') as log_file:
+                retcode = subprocess.call(command,
+                                          stdout=log_file, stderr=log_file)
+
+        if 0 != retcode:
+            message = ("Execution of {0} failed" +
+                       ("" if log_to is None else ", see the log for details"))
+
+            fail_with_error(message.format(command[0]))
+
+    def exec_chroot(self, command, log_to, action):
+        """Executes a command with chroot. Accepts same parameters as
+        exec_command() method.
+        """
+
+        command = ['chroot', self.chroot_dir, 'sh', '-c',
+                   'cd {0}; {1}'.format(self.chroot_src_path, command)]
+
+        self.exec_command(command, log_to, action)
+
+    def start_download(self):
+        """Downloads the sources archive with wget.
+        """
+
+        ext = 'xz' if self.version[0] == '2' else 'bz2'
+        filename = "vlc-{0}.tar.{1}".format(self.version, ext)
+
+        self.download_url = "/".join((config['download_url'],
+                                      self.version, filename))
+        self.archive_path = os.path.join(options.build_dir, filename)
+
+        command = ['wget', '-c', '-P', options.build_dir, '-O',
+                   self.archive_path, self.download_url]
+
+        self.debug_command(command)
+
+        if options.verbose:
+            output = None
+        else:
+            output = subprocess.PIPE
+
+        self.download_proc = subprocess.Popen(command,
+                                              stdout=output, stderr=output)
 
         self.build_logger.info("Starting download from " + self.download_url)
 
@@ -65,67 +137,68 @@ class Builder(object):
                 time.sleep(.1)
             elif retval != 0:
                 fail_with_error("Download failed :-(")
+            else:
+                return
 
+    @build_state('jail_created')
     def create_jail(self):
         """Creates a chroot jail with debootstrap.
         """
 
-        debootsrap_log_path = os.path.join(self.log_dir, 'debootstrap.log')
-
-        self.build_logger.info("Creating chroot jail for VLC {0}, see {1} for "
-                               "details"
-                               .format(self.version, debootsrap_log_path))
-
         command = ['debootstrap']
 
         if 'dependencies' in self.version_config:
-            command.append('--include=' +
-                           ",".join(self.version_config['dependencies']))
+            deps = ['build-essential'] + self.version_config['dependencies']
+            command.append('--include=' + ",".join(set(deps)))
 
         if 'arch' in self.version_config:
-            command.append('--arch=' +
-                           self.version_config['arch'])
+            command.append('--arch=' + self.version_config['arch'])
 
-        command += [self.version_config['distr'], self.build_dir]
+        command += [self.version_config['distr'], self.chroot_dir]
 
-        self.debug_command(command)
+        self.exec_command(command, log_to='debootstrap.log',
+                          action="Creating chroot jail for ")
 
-        with open(debootsrap_log_path, 'wt') as log_file:
-            if 0 != subprocess.call(command, stdout=log_file, stderr=log_file):
-                fail_with_error(
-                    "Execution of {0} failed, see the log for details"
-                    .format('debootstrap'))
-
-    def configure(self):
-        """Unpacks the downloaded archive and launches configure.
+    @build_state('source_unpacked')
+    def unpack(self):
+        """Unpacks the downloaded sources archive.
         """
 
         self.build_logger.info("Unpacking the downloaded archive")
 
-        configure_log_path = os.path.join(self.log_dir, 'configure.log')
+        command = ['tar', '-C',
+                   os.path.join(self.chroot_dir, 'usr/local/src/'),
+                   '-xf', self.archive_path]
 
-        self.build_logger.info("Configuring VLC, see {0} for details"
-                               .format(configure_log_path))
+        self.exec_command(command)
 
-        # Starting ./configure
-        # vlc_dir = '/usr/local/src/vlc-' + self.version
-        #
-        # command = ['chroot', self.build_dir,
-        # os.path.join(vlc_dir, 'configure')]
+    @build_state('configured')
+    def configure(self):
+        """Launches the `configure` script from jail.
+        """
 
+        configure_args = self.version_config.get('configure', '')
+        command = './configure --prefix=/usr ' + configure_args
+
+        self.exec_chroot(command, log_to='configure.log', action="configuring")
+
+    @build_state('compiled')
     def make(self):
-        pass
+        self.exec_chroot('make', log_to='make.log', action="compiling")
 
+    @build_state('installed')
     def install(self):
-        pass
+        self.exec_chroot('make install', log_to='install.log',
+                         action="installing")
 
     def run(self):
-        """This makes the whole magic.
+        """This one makes the whole magic.
         """
 
         self.start_download()
         self.create_jail()
         self.finish_download()
+        self.unpack()
         self.configure()
         self.make()
         self.install()
